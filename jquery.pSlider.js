5cb610951043c300e337a6a80ea76b5e15187857
saikit <saikit@areyoudesign.com>
2011-10-16 19:13:39 -0400

    Fixed scope issues.

--- jquery.pSlider.js
+++ jquery.pSlider.js
@@ -22,39 +22,22 @@ Website : http://areyoudesign.com/blog
 		animSpeed: 200, // animation speed for click event (ms)
 		arrowSpeed: 200, // set the interval for holding down the arrow buttons (ms)
 		thumb: 36, // size of the thumb (px)
-		flip: null, // percentage value the number is in a flipped state
+		flip: 80, // percentage value the number is in a flipped state
 		array: [], // display values matched against an array
 		onLoad: {}, // callback function after initializing slider
 		onMove: {}, // callback function that run while slider in motion
 		onFinish: {} // callback function that run after end of each action
 	}
+	
+	var int = false; // flag that starts and stop the onArrowClick handler
 
 $.fn.pSlider = function ( option ) {
-	
-	var opt = $.extend( defaults, option );
-	var $el = $(this);
 
-	var slider = (function() {
+	var slider = function (element, option) {
 	
-		/* constants and measurements */	
+		var $el = element;
+		var opt = $.extend({}, defaults, option);
 		var timer = 0; // define setInterval
-		var int = false; // flag that starts and stop the onArrowClick handler
-			
-		var range = opt.max - opt.min, // the range
-			indexes = range/opt.step, // total number of positions in a slider
-			tAdjust = (opt.length - opt.thumb) / opt.length; // adjusting position so that slider is correctly positioned depending on size
-			
-		var values = [opt.min]; // array of values in a slider, starting with opt.min
-			for(i = 0, len = indexes; i<len; i++) {
-				values[i + 1] = values[i] + opt.step;
-			};
-			
-		var positions = []; // array of positions in a slider, set by percentage
-			for(i = 0, len = indexes; i<=len; i++) {
-				positions[i] = (i/indexes) * 100;
-			};
-			
-		var data = {}; // object of slider data that can be accessed by the callback function
 		
 		init = function () {
 			buildSlider();
@@ -64,16 +47,43 @@ $.fn.pSlider = function ( option ) {
 		/* construct slider */
 		
 		buildSlider = function() {
-			$slider = $('<span class="pS-slider">'); 
-			$rail = $('<span class="pS-rail">').data({'status': 'ready'}).appendTo($slider);
-			$thumb = $('<span class="pS-thumb">').data({'status': 'ready'}).appendTo(this.$rail);
-			$progressBar = $('<span class="pS-progressBar">').appendTo(this.$rail);
-			$number = $('<span class="pS-number">').appendTo(this.$rail);
-			$cap = $('<span class="pS-cap-min"></span><span class="pS-cap-max"></span>').appendTo(this.$rail);
-			$arrows = $('<div class="pS-arrows">').appendTo($slider);
-			$up = $('<span class="pS-btn pS-btn-up">+</span>').appendTo($arrows);
-			$down = $('<span class="pS-btn pS-btn-down">&ndash;</span>').appendTo($arrows);				
+		
+			/* constants and measurements */
+			var range = opt.max - opt.min; // the range
 			
+			opt.indexes = range/opt.step, // total number of positions in a slider
+			opt.tAdjust = (opt.length - opt.thumb) / opt.length; // adjusting position so that slider is correctly positioned depending on size
+				
+			opt.values = [opt.min]; // array of values in a slider, starting with opt.min
+			for(i = 0, len = opt.indexes; i<len; i++) {
+				opt.values[i + 1] = opt.values[i] + opt.step;
+			};
+				
+			opt.positions = []; // array of positions in a slider, set by percentage
+			for(i = 0, len = opt.indexes; i<=len; i++) {
+				opt.positions[i] = (i/opt.indexes) * 100;
+			};
+				
+			var data = {}; // object of slider data that can be accessed by the callback function
+		
+		
+			/* build the slider */
+			var $slider = $('<span class="pS-slider">'),
+				$rail = $('<span class="pS-rail">').data({'status': 'ready'}).appendTo($slider),
+				$thumb = $('<span class="pS-thumb">').data({'status': 'ready'}).appendTo($rail),
+				$progressBar = $('<span class="pS-progressBar">').appendTo($rail),
+				$number = $('<span class="pS-number">').appendTo($rail),
+				$cap = $('<span class="pS-cap-min"></span><span class="pS-cap-max"></span>').appendTo($rail),
+				$arrows = $('<div class="pS-arrows">').appendTo($slider),
+				$up = $('<span class="pS-btn pS-btn-up">+</span>').appendTo($arrows),
+				$down = $('<span class="pS-btn pS-btn-down">&ndash;</span>').appendTo($arrows);				
+			
+			/* store slider components in data in order for reference */
+			$el.data({$thumb : $thumb, $rail : $rail, $up : $up, $down : $down, $number : $number, $progressBar : $progressBar, opt : opt});
+			
+			/* use the options stored in data */
+			opt = $el.data('opt');
+					
 			if(opt.axis == 'y') {
 				$slider.addClass('pS-slider-y')
 				$rail.css({height : opt.length});
@@ -83,41 +93,42 @@ $.fn.pSlider = function ( option ) {
 				$slider.addClass('pS-slider-x')
 				$rail.css({width : opt.length});
 				$thumb.css({width : opt.thumb});	
-			}
+			};
 			
-			dataController(positions[$.inArray(opt.value, values)] * opt.length, 'onLoad');
+			dataController($el, opt.positions[$.inArray(opt.value, opt.values)] * opt.length, 'onLoad');
 			
 			$el.html($slider);
 		};
 		
 		initListeners = function () {
-			$rail.click(handlers.onRailClick);
-			$thumb.bind('mousedown touchstart click', handlers.onThumbDrag);
-			$number.bind('touchstart mousedown select', handlers.onThumbDrag);			
-			$up.bind('mousedown touchstart', {direction : 'up'}, handlers.onArrowClick);
-			$down.bind('mousedown touchstart', {direction : 'down'}, handlers.onArrowClick);
+			$el.data('$rail').click(handlers.onRailClick);
+			$el.data('$thumb').bind('mousedown touchstart click', handlers.onThumbDrag);
+			$el.data('$number').bind('touchstart mousedown select', handlers.onThumbDrag);			
+			$el.data('$up').bind('mousedown touchstart', {direction : 'up'}, handlers.onArrowClick);
+			$el.data('$down').bind('mousedown touchstart', {direction : 'down'}, handlers.onArrowClick);
 		};
 		
 		// all handlers use this to output slider position, value, and index
 		
-		dataController = function (coordinate, call) {
+		dataController = function (element, coordinate, call) {
 			coordinate = coordinate/opt.length;
+			opt = element.data('opt');
 			
 			if(coordinate < 0) {
-				var position = positions[0];
-				var value = values[0];
+				var position = opt.positions[0];
+				var value = opt.values[0];
 				var index = 0;
 			}
 			else if(coordinate >= 100) {
-				var position = positions[indexes];
-				var value = values[indexes];
-				var index = indexes;
+				var position = opt.positions[opt.indexes];
+				var value = opt.values[opt.indexes];
+				var index = opt.indexes;
 			}
 			else {
-				for(i = 0, len = indexes; i<len; i++) {
-					if(coordinate - positions[i] < 1/indexes || coordinate - positions[i] == 0) {
-						var position = positions[i];
-						var value = values[i];
+				for(i = 0, len = opt.indexes; i<len; i++) {
+					if(coordinate - opt.positions[i] < 1/opt.indexes || coordinate - opt.positions[i] == 0) {
+						var position = opt.positions[i];
+						var value = opt.values[i];
 						var index = i;
 						break;
 					}
@@ -129,89 +140,89 @@ $.fn.pSlider = function ( option ) {
 			
 			// position adjusted for slider
 			
-			position = position * tAdjust;
+			position = position * opt.tAdjust;
 			
 			if(opt.animate && call == 'onFinish' && int == false) {
-				animateSlider(position, value, index, call);
+				animateSlider(element, position, value, index, call);
 			}
 			else {
-				setSlider(position, value, index, call);
+				setSlider(element, position, value, index, call);
 			};
 		};
 		
 		// animation from clicking on rail
 		
-		animateSlider = function (position, value, index, call) {
-			_animNum = function () {	
+		animateSlider = function (element, position, value, index, call) {
+			_animNum = function () {
 				if(cVal == value) {
 					clearInterval(anim);
-					$rail.data({'status' : 'ready'});
+					element.data('$rail').data({'status' : 'ready'});
 				}
 				else {
 					len = Math.abs(cVal - value)
 					var _sAdjust = len < 10 ? 1 : Math.round(len/10);
 					cVal += cVal < value ? opt.step * _sAdjust : opt.step * -1 * _sAdjust;
-					$number.text(arrayVal(index, cVal));
-					callback(cVal, position, index, 'onMove');
+					element.data('$number').text(arrayVal(index, cVal));
+					callback(element, cVal, position, index, 'onMove');
 				}
 			}
 			
-			var cVal = data.value,
+			var cVal = element.data('data').value,
 				len = Math.abs(cVal - value),
 				animSpeed = opt.animSpeed/len,
 				anim = setInterval(_animNum, animSpeed);
 			
 			if(opt.axis == 'y') {
-				$thumb.animate({bottom : position + '%'}, opt.animSpeed);
-				$number.animate({bottom : position + '%'}, opt.animSpeed)
-				$progressBar.animate({height : position + '%'}, opt.animSpeed);
+				element.data('$thumb').animate({bottom : position + '%'}, opt.animSpeed);
+				element.data('$number').animate({bottom : position + '%'}, opt.animSpeed)
+				element.data('$progressBar').animate({height : position + '%'}, opt.animSpeed);
 			}
 			else {
-				$thumb.animate({left : position + '%'}, opt.animSpeed);
-				$number.animate({bottom : position + '%'}, opt.animSpeed)
-				$progressBar.animate({width : position + '%'}, opt.animSpeed);
+				element.data('$thumb').animate({left : position + '%'}, opt.animSpeed);
+				element.data('$number').animate({bottom : position + '%'}, opt.animSpeed)
+				element.data('$progressBar').animate({width : position + '%'}, opt.animSpeed);
 			}
 			
 			if(opt.flip && position >= opt.flip) {
-				$number.addClass('pS-flipped');
+				element.data('$number').addClass('pS-flipped');
 			}
 			else {
-				$number.removeClass('pS-flipped');
+				element.data('$number').removeClass('pS-flipped');
 			};
 			
-			callback(value, position, index, call);
-		}
+			callback(element, value, position, index, call);
+		};
 		
-		setSlider = function (position, value, index, call)
-		{
+		setSlider = function (element, position, value, index, call) {
 			if(opt.axis == 'y') {
-				$thumb.css({bottom : position + '%'});
-				$number.css({bottom : position + '%'}).text(arrayVal(index, value));
-				$progressBar.css({height : position + '%'});
+				element.data('$thumb').css({bottom : position + '%'});
+				element.data('$number').css({bottom : position + '%'}).text(arrayVal(index, value));
+				element.data('$progressBar').css({height : position + '%'});
 			}
 			else {
-				$thumb.css({left : position + '%'});
-				$number.css({left : position + '%'}).text(arrayVal(index, value));
-				$progressBar.css({width : position + '%'});
+				element.data('$thumb').css({left : position + '%'});
+				element.data('$number').css({left : position + '%'}).text(arrayVal(index, value));
+				element.data('$progressBar').css({width : position + '%'});
 			};
 			
 			if(opt.flip && position >= opt.flip) {
-				$number.addClass('pS-flipped');
+				element.data('$number').addClass('pS-flipped');
 			}
 			else {
-				$number.removeClass('pS-flipped');
+				element.data('$number').removeClass('pS-flipped');
 			};
 			
-			$rail.data({'status' : 'ready'});
-			callback(value, position, index, call)
+			element.data('$rail').data({'status' : 'ready'});
+			callback(element, value, position, index, call);
 		};
 		
 		handlers = {
 			onRailClick : function (e) {
-				if($(this).data('status') == 'ready' && $thumb.data('status') == 'ready') {
+				opt = $el.data('opt');
+				if($(this).data('status') == 'ready' && $el.data('$thumb').data('status') == 'ready') {
 					$(this).data({'status' : 'moving'});
-					var position = opt.axis == 'y' ? (opt.length - (e.pageY - $rail.offset().top)) * 100 : (opt.length - (e.pageX - $rail.offset().left)) * 100;
-					dataController(position, 'onFinish');
+					var position = opt.axis == 'y' ? (opt.length - (e.pageY - $el.data('$rail').offset().top)) * 100 : (opt.length - (e.pageX - $el.data('$rail').offset().left)) * 100;
+					dataController($el, position, 'onFinish');
 				}
 				return false;
 			},
@@ -237,10 +248,12 @@ $.fn.pSlider = function ( option ) {
 					
 				// add mousemove
 					
-				$(document).bind('mousemove touchmove', handlers.onRailMove);
+				$(document).bind('mousemove touchmove', {element: $el}, handlers.onRailMove);
 			},
 			
 			onRailMove : function (e) {
+				var $el = e.data.element;
+					opt = $el.data('opt');
 				e.preventDefault();
 				e.stopPropagation();
 				if(e.type == 'touchmove') {
@@ -253,18 +266,19 @@ $.fn.pSlider = function ( option ) {
 					}
 				}
 				
-				$rail.data({'status' : 'moving'});
+				$el.data('$rail').data({'status' : 'moving'});
 				
-				var position = opt.axis == 'y' ? (opt.length - (e.pageY - $rail.offset().top)) * 100 : (opt.length - (e.pageX - $rail.offset().left)) * 100;
+				var position = opt.axis == 'y' ? (opt.length - (e.pageY - $el.data('$rail').offset().top)) * 100 : (opt.length - (e.pageX - $el.data('$rail').offset().left)) * 100;
 					
-				dataController(position, 'onMove');
+				dataController($el, position, 'onMove');
 				
 				// add mouseup on any part of document
 				
-				$(this).bind('mouseup touchend', handlers.onMouseUp);
+				$(this).bind('mouseup touchend', {element: $el}, handlers.onMouseUp);
 			},
 			
 			onMouseUp : function (e) {
+				var $el = e.data.element;
 				if(e.type == 'touchend') {
 					$(this).unbind('touchmove touchend');
 				}
@@ -272,23 +286,23 @@ $.fn.pSlider = function ( option ) {
 					$(this).unbind('mouseup mousemove');
 				};
 				
-				$thumb.removeClass('pS-dragging');
+				$el.data('$thumb').removeClass('pS-dragging');
 				
 				// prevent iPhone from triggering click event
-				setTimeout( function () { $thumb.data({'status' : 'ready'}) }, 1000);
+				setTimeout( function () { $el.data('$thumb').data({'status' : 'ready'}) }, 1000);
 			},
 			
 			onArrowClick : function (e) {
+				int = true;
 				$(this).addClass('pS-arrowDown');
 				
-				if(e.type == "touchstart")
+				if(e.type == "touchstart") {
 					$(this).unbind('mousedown');
-
-				int = true;
-				plusVal($(this), e.data.direction)
+				}
+				
+				plusVal($el, $(this), e.data.direction)
 					
 				$(document).bind('mouseup touchend', handlers.onArrowRelease);
-				
 			},
 			
 			onArrowRelease : function (e) {
@@ -314,12 +328,13 @@ $.fn.pSlider = function ( option ) {
 		
 		// Manage arrow keys
 		
-		plusVal = function (obj, direction) 
+		plusVal = function (element, obj, direction) 
 		{	
+			opt = element.data('opt');
 			clearInterval(timer);
 			
 			if(int == true && direction) {
-				timer = setInterval( function () { plusVal(obj, direction) }, opt.arrowSpeed );
+				timer = setInterval( function () { plusVal(element, obj, direction) }, opt.arrowSpeed );
 			}
 			else
 			{
@@ -327,13 +342,13 @@ $.fn.pSlider = function ( option ) {
 				return;
 			}
 				
-			var value = data.value;
+			var value = element.data('data').value;
 			
 			if(direction == 'up' && value < opt.max) {
-				dataController(positions[$.inArray(value, values) + 1] * opt.length, 'onFinish');
+				dataController(element, opt.positions[$.inArray(value, opt.values) + 1] * opt.length, 'onFinish');
 			}
 			else if(direction == 'down' && value > opt.min) {
-				dataController(positions[$.inArray(value, values) - 1] * opt.length, 'onFinish');
+				dataController(element, opt.positions[$.inArray(value, opt.values) - 1] * opt.length, 'onFinish');
 			}
 			else {
 				return;
@@ -344,27 +359,32 @@ $.fn.pSlider = function ( option ) {
 		
 		// update public dataset and run callback function
 		
-		callback = function (value, position, index, call) {
+		callback = function (element, value, position, index, call) {
 			data = {
-				value : value,
-				arrayValue : $.isArray(opt.array) ? arrayVal(value) : '',
-				percentage : Math.round(position / tAdjust),
-				index : index
+				value : value, // the displayed slider value
+				arrayValue : $.isArray(opt.array) ? arrayVal(value) : value, // the displayed value matched against opt.array
+				percentage : Math.round(position / opt.tAdjust), // slider position relative to slider length
+				index : index // slider index
 			};
 			
+			// update dataset in element
+			element.data({data : data});
+			
+			// callback
 			if(typeof(opt[call]) === 'function')
 				opt[call].call(this, data);
 		};
-	
+		
 		return {
 			init : init
 		};
-	
-	})();
+	};
 
 	return this.each( function ()
 	{
-		slider.init();
+		var element = $(this);
+		var s = new slider(element, option);
+		s.init();
 	});		 
 }
 	
